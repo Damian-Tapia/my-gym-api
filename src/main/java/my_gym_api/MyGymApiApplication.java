2cd86c8940b718a3b484148a7efbea4029cc7df5
Damian-Tapia <damian.tapia45@gmail.com>
2026-09-23 13:37:29 -0600

    initial setup

---
+++ src/main/java/my_gym_api/MyGymApiApplication.java
@@ -0,0 +1,13 @@
+package my_gym_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyGymApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyGymApiApplication.class, args);
+	}
+
+}
---
+++ src/main/java/my_gym_api/entity/Routine.java
@@ -0,0 +1,5 @@
+package my_gym_api.entity;
+
+public class Routine {
+    
+}
