177c44d3067f52a2501d7eb48851c28e8edd8972
Damian-Tapia <damian.tapia45@gmail.com>
2026-09-24 13:28:50 -0600

    formated

--- src/main/java/my_gym_api/entity/Routine.java
+++ src/main/java/my_gym_api/entity/Routine.java
@@ -9,34 +9,38 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "routines") 
+@Table(name = "routines")
 public class Routine {
-    @Id 
-    private UUID              id;
-    @Column(name="user_id")
-    private UUID              userId;
+    @Id
+    private UUID id;
+    @Column(name = "user_id")
+    private UUID userId;
 
-    @Column(nullable=false)
-    private String            name;
+    @Column(nullable = false)
+    private String name;
 
-    @Column(name="is_active")
-    private Boolean           isActive;
-    
-    @Column(name="created_at")
-    private OffsetDateTime    createdAt;
+    @Column(name = "is_active")
+    private Boolean isActive;
+
+    @Column(name = "created_at")
+    private OffsetDateTime createdAt;
 
     public UUID getId() {
         return id;
     }
+
     public UUID getUserId() {
         return userId;
     }
+
     public String getName() {
         return name;
     }
+
     public Boolean getIsActive() {
         return isActive;
     }
+
     public OffsetDateTime getCreatedAt() {
         return createdAt;
     }
@@ -44,15 +48,19 @@ public class Routine {
     public void setId(UUID id) {
         this.id = id;
     }
+
     public void setUserId(UUID userId) {
         this.userId = userId;
     }
+
     public void setName(String name) {
         this.name = name;
     }
+
     public void setIsActive(Boolean isActive) {
         this.isActive = isActive;
     }
+
     public void setCreatedAt(OffsetDateTime createdAt) {
         this.createdAt = createdAt;
     }
