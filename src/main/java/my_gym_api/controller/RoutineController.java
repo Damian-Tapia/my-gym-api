f21aff9cfe232f317e812d2b5a306f06af6da86d
Damian-Tapia <damian.tapia45@gmail.com>
2026-09-23 21:29:18 -0600

    created: routine getByID

--- src/main/java/my_gym_api/controller/RoutineController.java
+++ src/main/java/my_gym_api/controller/RoutineController.java
@@ -1,8 +1,12 @@
 package my_gym_api.controller;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,7 +14,7 @@ import my_gym_api.entity.Routine;
 import my_gym_api.repository.RoutineRepository;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/routines")
 public class RoutineController {
 
     private final RoutineRepository routineRepository;
@@ -19,9 +23,16 @@ public class RoutineController {
         this.routineRepository = routineRepository;
     }
 
-    @GetMapping("/routines")
+    @GetMapping()
     public List<Routine> getRoutines() {
         return routineRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Routine> getRoutineById(@PathVariable UUID id) {
+        return routineRepository.findById(id)
+                .map(routine -> ResponseEntity.ok(routine))
+                .orElse(ResponseEntity.notFound().build());
     }
 
 }
